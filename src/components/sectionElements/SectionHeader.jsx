ca2a0d2bae51b23aad2d50ef5ed21eed0c697ea6
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-11-01 12:07:10 -0300

    fix: Remove minitag

--- src/components/sectionElements/SectionHeader.jsx
+++ src/components/sectionElements/SectionHeader.jsx
@@ -56,18 +56,23 @@ export default function SectionHeader({
   const Content = (
     <div className={`${usage} ${className}`}>
       <div>
-        <div className={`${miniTitleSpace}`}>
-          <div
-            className={`py-[4px] font-semibold px-[12px] font-mainFont text-paragraph2 rounded-md inline-block mb-[16px] ${miniTitleOrientation} ${miniTitleBgColor}`}
-          >
-            <p className={`${miniTitleTextColor} uppercase`}>{miniTitle}</p>
+        {/* Só renderiza o miniTitle se for true (ou tiver valor válido) */}
+        {miniTitle && (
+          <div className={`${miniTitleSpace}`}>
+            <div
+              className={`py-[4px] font-semibold px-[12px] font-mainFont text-paragraph2 rounded-md inline-block mb-[16px] ${miniTitleOrientation} ${miniTitleBgColor}`}
+            >
+              <p className={`${miniTitleTextColor} uppercase`}>{miniTitle}</p>
+            </div>
           </div>
-        </div>
+        )}
+
         <h1
           className={`${titleColor} ${titleOrientation} text-title4 bg-transparent leading-[34px] tablet1:leading-[42px] tablet1:text-title5 font-mainFont font-bold mb-[16px]`}
         >
           {sectionHeaderTitle}
         </h1>
+
         <p
           className={`text-title1 font-secondFont leading-[26px] bg-transparent ${marginBottomOption} ${subtitleOrientation} ${subtitleColor}`}
         >
