574e7e7bccbb62eed765151e4c88d68a22dc3030
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-11-13 04:58:01 -0300

    fix: Reflow

--- src/pages/Lp01.jsx
+++ src/pages/Lp01.jsx
@@ -9,27 +9,45 @@ import SocialProof from "../components/sections/lp01/SocialProof";
 import Client from "../components/sections/lp01/Client";
 
 export default function Lp01() {
-  useEffect(() => {
-    document.title = contentLp01.infos.title;
-    const metaDescription = document.querySelector("meta[name='description']");
-    if (metaDescription) {
-      metaDescription.setAttribute("content", contentLp01.infos.description);
-    } else {
-      const meta = document.createElement("meta");
-      meta.name = "description";
-      meta.content = contentLp01.infos.description;
-      document.head.appendChild(meta);
-    }
+  // useEffect(() => {
+  //   document.title = contentLp01.infos.title;
+  //   const metaDescription = document.querySelector("meta[name='description']");
+  //   if (metaDescription) {
+  //     metaDescription.setAttribute("content", contentLp01.infos.description);
+  //   } else {
+  //     const meta = document.createElement("meta");
+  //     meta.name = "description";
+  //     meta.content = contentLp01.infos.description;
+  //     document.head.appendChild(meta);
+  //   }
 
-    const metaKeywords = document.querySelector("meta[name='keywords']");
-    if (metaKeywords) {
-      metaKeywords.setAttribute("content", contentLp01.infos.keywords);
-    } else {
-      const meta = document.createElement("meta");
-      meta.name = "keywords";
-      meta.content = contentLp01.infos.keywords;
-      document.head.appendChild(meta);
-    }
+  //   const metaKeywords = document.querySelector("meta[name='keywords']");
+  //   if (metaKeywords) {
+  //     metaKeywords.setAttribute("content", contentLp01.infos.keywords);
+  //   } else {
+  //     const meta = document.createElement("meta");
+  //     meta.name = "keywords";
+  //     meta.content = contentLp01.infos.keywords;
+  //     document.head.appendChild(meta);
+  //   }
+  // }, []);
+  useEffect(() => {
+    requestIdleCallback(() => {
+      document.title = contentLp01.infos.title;
+
+      const setMeta = (name, content) => {
+        let tag = document.querySelector(`meta[name='${name}']`);
+        if (!tag) {
+          tag = document.createElement("meta");
+          tag.name = name;
+          document.head.appendChild(tag);
+        }
+        tag.content = content;
+      };
+
+      setMeta("description", contentLp01.infos.description);
+      setMeta("keywords", contentLp01.infos.keywords);
+    });
   }, []);
 
   return (
