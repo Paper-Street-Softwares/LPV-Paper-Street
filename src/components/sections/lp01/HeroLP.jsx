e604090d79c3fdd14a3a61aa24e2bfc1228d614f
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-11-13 05:10:27 -0300

    fix: Teste

--- src/components/sections/lp01/HeroLP.jsx
+++ src/components/sections/lp01/HeroLP.jsx
@@ -1,7 +1,7 @@
 
 import { useNavigate } from "react-router-dom";
 import FullImg from "../../sectionElements/Lp01/hero/FullImg";
-import DefaultHero from "../../sectionElements/Lp01/hero/DefaultHero";
+// import DefaultHero from "../../sectionElements/Lp01/hero/DefaultHero";
 
 export default function HeroLP({ fullImg }) {
   const navigate = useNavigate();
