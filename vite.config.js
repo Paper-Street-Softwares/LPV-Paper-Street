a9b9ae5140281153bb5fb026fa5b6f8db87fd971
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-11-12 22:53:16 -0300

    fix: Teste

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,16 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react-swc'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   optimizeDeps: {
-//     include: ['primereact/dialog', 'primereact/button'],
-//   },
-// })
-
-// vite.config.js
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 import vitePluginCritters from "vite-plugin-critters";
 
 export default defineConfig({
   plugins: [
     react(),
     vitePluginCritters({
-      preload: "media", // ou 'swap'
+      preload: "media", // ou 'swap' (teste qual dá melhor pontuação)
+      compress: true, // minifica CSS inline
     }),
   ],
+  optimizeDeps: {
+    include: ["primereact/dialog", "primereact/button"],
+  },
 });
