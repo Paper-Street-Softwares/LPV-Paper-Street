43eb361699495364be59275ac83d1812e6a3a00b
Edison Matos <edison.matos@live.com>
2025-11-26 16:34:07 -0300

    feat: Corrige opacidade

--- src/components/interactives/Button.jsx
+++ src/components/interactives/Button.jsx
@@ -174,7 +174,7 @@ export default function Button({
           MODAL
       -------------------------- */}
       {isModalOpen && (
-        <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-[9999]">
+        <div className="fixed inset-0 bg-black/90 flex items-center justify-center z-[9999]">
           <div className="bg-white w-[90%] max-w-[420px] p-6 rounded-xl shadow-xl relative">
             {/* BOTÃO DE FECHAR */}
             <button
