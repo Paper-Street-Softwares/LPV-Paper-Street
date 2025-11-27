f3cf1c3a5cc4ef3bbdb49991e4daf5415a0fa674
Edison Matos <edison.matos@live.com>
2025-11-27 11:27:02 -0300

    feat: Adiciona botao flutuante

--- src/pages/Lp01.jsx
+++ src/pages/Lp01.jsx
@@ -7,49 +7,9 @@ import { useEffect } from "react";
 import VideoCarousel from "../components/interactives/CarouselVideos";
 import SocialProof from "../components/sections/lp01/SocialProof";
 import Client from "../components/sections/lp01/Client";
+import WhatsappAnimated from "../components/interactives/WhatsappAnimated";
 
 export default function Lp01() {
-  // useEffect(() => {
-  //   document.title = contentLp01.infos.title;
-  //   const metaDescription = document.querySelector("meta[name='description']");
-  //   if (metaDescription) {
-  //     metaDescription.setAttribute("content", contentLp01.infos.description);
-  //   } else {
-  //     const meta = document.createElement("meta");
-  //     meta.name = "description";
-  //     meta.content = contentLp01.infos.description;
-  //     document.head.appendChild(meta);
-  //   }
-
-  //   const metaKeywords = document.querySelector("meta[name='keywords']");
-  //   if (metaKeywords) {
-  //     metaKeywords.setAttribute("content", contentLp01.infos.keywords);
-  //   } else {
-  //     const meta = document.createElement("meta");
-  //     meta.name = "keywords";
-  //     meta.content = contentLp01.infos.keywords;
-  //     document.head.appendChild(meta);
-  //   }
-  // }, []);
-  // useEffect(() => {
-  //   requestIdleCallback(() => {
-  //     document.title = contentLp01.infos.title;
-
-  //     const setMeta = (name, content) => {
-  //       let tag = document.querySelector(`meta[name='${name}']`);
-  //       if (!tag) {
-  //         tag = document.createElement("meta");
-  //         tag.name = name;
-  //         document.head.appendChild(tag);
-  //       }
-  //       tag.content = content;
-  //     };
-
-  //     setMeta("description", contentLp01.infos.description);
-  //     setMeta("keywords", contentLp01.infos.keywords);
-  //   });
-  // }, []);
-
   return (
     <div className="bg-bgFixedLight">
       <HeroLP fullImg={true} />
@@ -58,7 +18,7 @@ export default function Lp01() {
       <Client />
       <AboutParalaxeLP logo={true} />
       <FooterLP />
-      {/* <FloatingWhatsappButton /> */}
+      <WhatsappAnimated />
     </div>
   );
 }
