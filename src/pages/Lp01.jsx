cee35f4f32a731f2fbfabf0414f84c2b4a541f6f
Gabriel Souza <gabrielsouza65115@gmail.com>
2025-12-04 11:51:53 -0300

    fix: Js retornado

--- src/pages/Lp01.jsx
+++ src/pages/Lp01.jsx
@@ -1,11 +1,20 @@
 import HeroLP from "../components/sections/lp01/HeroLP";
 import WhatsappAnimated from "../components/interactives/WhatsappAnimated";
 import React from "react";
-import VideoCarousel from "../components/interactives/CarouselVideos";
-import SocialProof from "../components/sections/lp01/SocialProof";
-import Client from "../components/sections/lp01/Client";
-import AboutParalaxeLP from "../components/sections/lp01/AboutParalaxeLP";
-import FooterLP from "../components/sections/lp01/FooterLP";
+
+const VideoCarousel = React.lazy(() =>
+  import("../components/interactives/CarouselVideos")
+);
+const SocialProof = React.lazy(() =>
+  import("../components/sections/lp01/SocialProof")
+);
+const Client = React.lazy(() => import("../components/sections/lp01/Client"));
+const AboutParalaxeLP = React.lazy(() =>
+  import("../components/sections/lp01/AboutParalaxeLP")
+);
+const FooterLP = React.lazy(() =>
+  import("../components/sections/lp01/FooterLP")
+);
 
 export default function Lp01() {
   return (
