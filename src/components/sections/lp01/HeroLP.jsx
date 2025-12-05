3e11e939b687840aca35c8f2aea8e25b02cd709a
Gabriel Souza <gabrielsouza65115@gmail.com>
2025-12-05 11:05:55 -0300

    fix: Pontuacoes definidas de lcp

--- src/components/sections/lp01/HeroLP.jsx
+++ src/components/sections/lp01/HeroLP.jsx
@@ -1,10 +1,6 @@
-
-import { useNavigate } from "react-router-dom";
 import FullImg from "../../sectionElements/Lp01/hero/FullImg";
 // import DefaultHero from "../../sectionElements/Lp01/hero/DefaultHero";
 
 export default function HeroLP({ fullImg }) {
-  const navigate = useNavigate();
-
   return fullImg ? <FullImg obs={false} /> : <DefaultHero />;
 }
