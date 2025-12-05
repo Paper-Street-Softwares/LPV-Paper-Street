4c65da95d3adb8373748fb3a92e9651cefb818e9
Gabriel Souza <gabrielsouza65115@gmail.com>
2025-12-05 08:37:10 -0300

    fix: Corrigindo index

--- src/pages/Lp01.jsx
+++ src/pages/Lp01.jsx
@@ -1,6 +1,6 @@
 import HeroLP from "../components/sections/lp01/HeroLP";
 import WhatsappAnimated from "../components/interactives/WhatsappAnimated";
-import React, { Suspense, useEffect, useState } from "react";
+import React from "react";
 
 const VideoCarousel = React.lazy(() =>
   import("../components/interactives/CarouselVideos")
@@ -17,32 +17,14 @@ const FooterLP = React.lazy(() =>
 );
 
 export default function Lp01() {
-  const [showBelow, setShowBelow] = useState(false);
-
-  useEffect(() => {
-    const onScroll = () => {
-      if (window.scrollY > 200) {
-        setShowBelow(true);
-        window.removeEventListener("scroll", onScroll);
-      }
-    };
-
-    window.addEventListener("scroll", onScroll);
-    return () => window.removeEventListener("scroll", onScroll);
-  }, []);
-
   return (
     <div className="bg-bgFixedLight">
       <HeroLP fullImg={true} />
-
-      <Suspense fallback={null}>
-        {showBelow && <VideoCarousel />}
-        {showBelow && <SocialProof />}
-        {showBelow && <Client />}
-        {showBelow && <AboutParalaxeLP logo={true} />}
-        {showBelow && <FooterLP />}
-      </Suspense>
-
+      <VideoCarousel />
+      <SocialProof />
+      <Client />
+      <AboutParalaxeLP logo={true} />
+      <FooterLP />
       <WhatsappAnimated />
     </div>
   );
