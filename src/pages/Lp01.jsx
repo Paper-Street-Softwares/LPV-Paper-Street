f5eb5b25e3e1d819db38b1e2151db3fcda2997f8
Gabriel Souza <gabrielsouza65115@gmail.com>
2026-01-17 08:43:11 -0300

    fix: Tags

--- src/pages/Lp01.jsx
+++ src/pages/Lp01.jsx
@@ -1,27 +1,32 @@
-import HeroLP from "../components/sections/lp01/HeroLP";
-import React from "react";
+import HeroLP from '../components/sections/lp01/HeroLP'
+import React from 'react'
 
-const VideoCarousel = React.lazy(() =>
-  import("../components/interactives/CarouselVideos")
-);
-const SocialProof = React.lazy(() =>
-  import("../components/sections/lp01/SocialProof")
-);
-const Client = React.lazy(() => import("../components/sections/lp01/Client"));
-const AboutParalaxeLP = React.lazy(() =>
-  import("../components/sections/lp01/AboutParalaxeLP")
-);
-const FooterLP = React.lazy(() =>
-  import("../components/sections/lp01/FooterLP")
-);
+const VideoCarousel = React.lazy(
+  () => import('../components/interactives/CarouselVideos'),
+)
+const SocialProof = React.lazy(
+  () => import('../components/sections/lp01/SocialProof'),
+)
+const Client = React.lazy(() => import('../components/sections/lp01/Client'))
+const AboutParalaxeLP = React.lazy(
+  () => import('../components/sections/lp01/AboutParalaxeLP'),
+)
+const FooterLP = React.lazy(
+  () => import('../components/sections/lp01/FooterLP'),
+)
 
-const WhatsappAnimated = React.lazy(() =>
-  import("../components/interactives/WhatsappAnimated")
-);
+const WhatsappAnimated = React.lazy(
+  () => import('../components/interactives/WhatsappAnimated'),
+)
 
 export default function Lp01() {
   return (
     <div className="bg-bgFixedLight">
+      {/* <div className="bg-primary flex justify-center">
+        <h1 className="text-title2">
+          Para Advogados(a) que precisam captar clientes na internet
+        </h1>
+      </div> */}
       <HeroLP fullImg={true} />
       <VideoCarousel />
       <SocialProof />
@@ -30,5 +35,5 @@ export default function Lp01() {
       <FooterLP />
       <WhatsappAnimated />
     </div>
-  );
+  )
 }
