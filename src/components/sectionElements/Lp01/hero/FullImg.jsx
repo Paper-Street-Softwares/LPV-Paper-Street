53eac89070f7fcf20e03b5bd8b9f01c1473ee3d6
Gabriel Souza <gabrielsouza65115@gmail.com>
2026-01-19 09:51:48 -0300

    fix: webp

--- src/components/sectionElements/Lp01/hero/FullImg.jsx
+++ src/components/sectionElements/Lp01/hero/FullImg.jsx
@@ -64,7 +64,7 @@ function FullImg({ obs }) {
                       <div className="flex flex-col items-center justify-center w-full gap-12 phone2:w-full tablet2:w-auto">
                         <MotionDivDownToUp>
                           <img
-                            src="/mockup.png"
+                            src="/mockup.webp"
                             alt="Site de Advogado"
                             width="500"
                             height="256"
@@ -173,7 +173,7 @@ function FullImg({ obs }) {
                       <div className="flex flex-col justify-center w-full gap-20 items-center">
                         <MotionDivDownToUp>
                           <img
-                            src="/mockup.png"
+                            src="/mockup.webp"
                             alt="Site de Advogado"
                             width="500"
                             height="256"
